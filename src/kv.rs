54e8768bdfa43d95b3454dfb4835467fe4e5f96f
amirrezaask <raskarpour@gmail.com>
2021-09-26 23:58:20 +0330

    working version

--- src/kv.rs
+++ src/kv.rs
@@ -1,33 +1,46 @@
-use std::{collections::HashMap, error::Error, hash::Hash};
+use std::{collections::HashMap, error::Error, fmt::Display, hash::Hash};
 
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, RwLock};
 
 #[derive(Debug)]
 pub struct StorageError(String);
+impl Display for StorageError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "storage layer error: {}", &self.0)
+    }
+}
 impl Error for StorageError {}
 
 pub trait Storage<K, V> {
     fn get(&self, key: K) -> Result<V, StorageError>;
-    fn put(&mut self, key: K, value: V) -> Result<(), StorageError>;
+    fn put(&self, key: K, value: V) -> Result<(), StorageError>;
 }
 
 pub struct HashMapStorage<K, V>(Arc<RwLock<HashMap<K, V>>>);
+
 impl<K, V> HashMapStorage<K, V> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         HashMapStorage::<K, V>(Arc::new(RwLock::new(HashMap::new())))
     }
 }
-impl<K, V> Storage<K, V> for HashMapStorage<K, V> {
+impl<K, V> Storage<K, V> for HashMapStorage<K, V>
+where
+    K: Eq + Hash,
+    V: Clone,
+{
     fn get(&self, key: K) -> Result<V, StorageError> {
-        let hm: HashMap<K, V> = self.0.clone().read()?;
+        let arc = self.0.clone();
+        let hm = arc.read().unwrap();
+
         match hm.get(&key) {
-            Some(v) => Ok(v),
-            None => Err(StorageError(String::from("key does exists"))),
+            Some(v) => Ok(v.clone()),
+            None => Err(StorageError(String::from("error key does not exists"))),
         }
     }
-    fn put(&mut self, key: K, val: V) -> Result<(), StorageError> {
-        let mut hm: HashMap<K, V> = &self.0.clone().write()?;
+    fn put(&self, key: K, val: V) -> Result<(), StorageError> {
+        let arc = self.0.clone();
+        let mut hm = arc.write().unwrap();
         hm.insert(key, val);
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
 #[macro_use]
 extern crate rocket;
-use rocket::{response::Responder, serde::json::Json};
+use kv::Storage;
+use rocket::{response::Responder, serde::json::Json, State};
 
-mod http;
 mod kv;
-use http::*;
 
 #[get("/<id>")]
-fn get(id: String) -> Json<Response<String>> {
-    KVResponse::new("OK".to_string(), "salam".to_string())
+fn get(storage: &State<kv::HashMapStorage<String, String>>, id: String) -> Json<String> {
+    match storage.get(id) {
+        Ok(v) => Json(v.to_string()),
+        Err(e) => Json(format!("{}", e)),
+    }
 }
 
-#[post("/")]
-fn set(id: String) -> Json<Response<String>> {}
+#[put("/<id>/<value>")]
+fn set(
+    storage: &State<kv::HashMapStorage<String, String>>,
+    id: String,
+    value: String,
+) -> Json<String> {
+    let res = storage.put(id, value);
+    match res {
+        Ok(_) => Json("OK".to_string()),
+        Err(e) => Json(format!("{}", e)),
+    }
+}
 
-#[delete("/<id>")]
-fn del(id: String) -> Json<Response<String>> {}
+// #[delete("/<id>")]
+// fn del(storage: &State<kv::HashMapStorage<String, String>>, id: String) -> Json<Response<String>> {
+//     match storage.get(id) {
+//         Ok(v) => Json(http::KVResponse::new(
+//             "OK".to_string(),
+//             String::from("Deleted"),
+//         )),
+//         Err(e) => Json(http::KVResponse::new("Error".to_string(), e.to_string())),
+//     }
+// }
 
 #[launch]
 fn launch() -> _ {
-    rocket::build().mount("/", routes![get, set, del])
+    rocket::build()
+        .manage(kv::HashMapStorage::<String, String>::new())
+        .mount("/", routes![get, set])
 }
